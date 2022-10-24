e6b4e8e47290ca2a7c4ca99aacef235f6c5a223b
hakunamatras <113973586+hakunamatras@users.noreply.github.com>
2022-10-24 06:44:29 +0200

    Add files via upload

---
+++ jack-main/jack-main/src/App.js
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+
+const App = () => {
+  return (
+    <>
+      <div className="home-page">
+        <section className="landing-left-section">
+          <div className="text">
+            <h1>hi</h1>
+            <h1>Im Juan</h1>
+            <h1>Webdeveloper</h1>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default App;
